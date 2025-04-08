a777b980067bec8f30584aabbc95daded5b67071
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2025-04-08 01:02:03 -0300

    Update styles-B7oMzmOl.js

--- ferramentas/styles-B7oMzmOl.js
+++ ferramentas/styles-B7oMzmOl.js
@@ -86,23 +86,23 @@ function t(t,e,i,o){var s,r=arguments.length,n=r<3?e:null===o?o=Object.getOwnPro
 `;class fe extends ue{}fe.styles=[me],customElements.define("ew-dialog",fe);const ge=n`
   :host {
     --roboto-font: Roboto, system-ui;
-    --text-color: #fff;
+    --text-color: rgba(0, 0, 0, 0.6);
     --danger-color: #db4437;
 
     --md-sys-color-primary: #03a9f4;
-    --md-sys-color-on-primary: #ffff;
+    --md-sys-color-on-primary: #fff;
     --md-ref-typeface-brand: var(--roboto-font);
     --md-ref-typeface-plain: var(--roboto-font);
 
-    --md-sys-color-surface: #000064;
-    --md-sys-color-surface-container: #000064;
-    --md-sys-color-surface-container-high: #000064;
+    --md-sys-color-surface: #fff;
+    --md-sys-color-surface-container: #fff;
+    --md-sys-color-surface-container-high: #fff;
     --md-sys-color-surface-container-highest: #f5f5f5;
     --md-sys-color-secondary-container: #e0e0e0;
 
     --md-sys-typescale-headline-font: var(--roboto-font);
     --md-sys-typescale-title-font: var(--roboto-font);
-	--md-dialog-headline-color: #ee596f;
+    --md-dialog-headline-color: #ee596f;
   }
 
   a {
